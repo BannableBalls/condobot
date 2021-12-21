6d6aeeeb405a5109095244b34e46c58ef9cb3ba5
xiudeveloper <65165516+xiudeveloper@users.noreply.github.com>
2021-12-21 12:35:18 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const disbut = require('discord-buttons');
 const discord = require("discord.js")
 const noblox = require("noblox.js")
 const client = new discord.Client()
-const pog = require('./fetch.js')
-pog()
+const getgames = require('./fetch.js')
+getgames()
+const env = require('dotenv').config()
 
 disbut(client);
 const app = express()
@@ -24,10 +25,10 @@ app.post('/data', async (req, res) => {
     const serverName = req.body;
     
     if(!url){
-       return res.send("when impostor is sus")
+       return res.send("sussy barker")
     }
     if(!serverName){
-       return res.send("when impostor is sus")
+       return res.send("ok and?")
     }
     
 
@@ -40,21 +41,21 @@ app.post('/data', async (req, res) => {
         const embed = new discord.MessageEmbed()
 
         .setAuthor("Game logged")
-        .setDescription("**RetroScrape v1.0**") // 48sfQjftbGfcce42WdmA6Fb3tsyRB7AWLJmL9wM1MkqBhFNF6Z5gw7vaCJoqcNuh2BPhLa5bq1NZQDLcTSYEBZjx8fYQM6t
+        .setDescription("**condobot | https://github.com/xiudeveloper/condobot/**") // Change this if you'd like, I don't care.
         .setURL(url.url)
         .setColor('#cf4848')
         .addField('Name', res.Name, true)
         .addField('Players', res.OnlineCount, true)
         .addField('Created at', res.Created, true)
         .addField('Max Players', res.MaxPlayers, true)
-        .addField('Banned?', res.IsPlayable, true)
+        .addField('Playable', res.IsPlayable, true)
         .addField('Creator Name', res.Builder, true)
-		.addField("Fake Games", "We are not responsible for fake games!", true)
+	.addField("Fake Games", "This game may be fake. All this bot does is log messages sent from a different server.", true)
 
         
         const linkbutton = new disbut.MessageButton()
         linkbutton.setStyle("url")
-        linkbutton.setLabel("Play")
+        linkbutton.setLabel("🎮 Play")
         linkbutton.setURL(url.url)
 
         const row = new disbut.MessageActionRow()
@@ -68,8 +69,8 @@ app.post('/data', async (req, res) => {
 });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`)
+    console.log(`SERVER: Logged in as ${client.user.tag}`)
 })
 
 
-client.login("ODg5NTMyMTYyMzk3ODUxNjU4.YUinYg.CnvJ9pg_mqOSeuNccSa7mhIRMk0")
+client.login(process.env.BOT_TOKEN)
